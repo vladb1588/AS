17b3914c5e7fdca83b7f95e819d68be3480a8dd5
Vlad B <vladbeldze@gmail.com>
2025-05-22 22:23:18 +0700

    1

--- lesson/js_lesson_6.js
+++ lesson/js_lesson_6.js
@@ -1,4 +1,21 @@
-// Урок 6: Функции
-// Задание: Напишите функцию, которая принимает два числа и возвращает их сумму. Вызовите функцию и выведите результат в консоль.
+// Урок 6: Циклы и итерации
+// Задание: Выведите элементы массива с помощью for, while и for...of.
 
-// Ваш код здесь
+const array = [1, 2, 3, 4, 5];
+
+// Цикл for
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+// Цикл while
+let j = 0;
+while (j < array.length) {
+    console.log(array[j]);
+    j++;
+}
+
+// Цикл for...of
+for (const element of array) {
+    console.log(element);
+}
